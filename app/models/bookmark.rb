0f71a5374e0aac746f9934d684fde22e65e2f696
Theo Chatonnet <theo.chatonnet@gmail.com>
2023-08-17 11:28:22 +0200

    new rails watch list

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'Ce film est déjà dans cette liste' }
+end
